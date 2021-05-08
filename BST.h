5a76432cf05a2cb61a61324ae718fc1bc18445aa
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-05-08 21:19:54 +0200

    constructor/descructor

--- BST.h
+++ BST.h
@@ -7,9 +7,22 @@ struct BST
 	int key;
 	BST* left;
 	BST* right;
+	BST(){
+		key = 0;
+		left = nullptr;
+		right = nullptr;
+	};
+	~BST(){
+		if(left){
+			delete left;
+		}
+		if(right){
+			delete right;
+		}
+	}
 };
 
-BST* insert_node(BST *&root, int key)
+BST* BST_insert_node(BST *&root, int key)
 {
 	if (!root)
 	{
@@ -21,32 +34,84 @@ BST* insert_node(BST *&root, int key)
 	}
 	else if (key <= root->key)
 	{
-		insert_node(root->left, key);
+		BST_insert_node(root->left, key);
 	}
 	else
 	{
-		insert_node(root->right, key);
+		BST_insert_node(root->right, key);
 	}
 	return root;
 }
 
-void displayTreeInorder(BST* root)
+BST* BST_search_node(BST *&root, int key)
+{
+	if(root == nullptr || root->key == key){
+		return root;
+	}
+	if(root->key < key){
+		return BST_search_node(root->right, key);
+	}
+	return BST_search_node(root->left,key);
+}
+
+BST* BST_delete_node(BST *&root, int key){
+	if(root == nullptr){
+		return nullptr;
+	}
+	if(root->key > key){
+		root->left = BST_delete_node(root->left,key);
+		return root;
+	}
+	else if(root->key < key){
+		root->right = BST_delete_node(root->right,key);
+		return root;
+	}
+	if(root->left == nullptr){ //tylko prawe dziecko
+		BST* temp = root->right;
+		delete root;
+		return temp;
+	} 
+	else if(root->right == nullptr){ //tylko lewe dziecko
+		BST* temp = root->left;
+		delete root;
+		return temp;
+	}
+	else{
+		BST* parent = root;
+		BST* successor = root->right;
+		while(successor->left != nullptr){ //wyszukiwanie następnika
+			parent = successor;
+			successor = successor->left;
+		}
+		if(parent != root){
+			parent->left = successor->right;
+		}
+		else{
+			parent->right = successor->right;
+		}
+		root->key = successor->key;
+		delete successor;
+		return root;
+	}
+}
+
+void BST_inorder(BST* root)
 {
 	if (root)
 	{
 		cout << root->key << " ";
-		displayTreeInorder(root->left);
-		displayTreeInorder(root->right);
+		BST_inorder(root->left);
+		BST_inorder(root->right);
 	}
 }
 
-void postorder(BST* root, int indent)
+void BST_postorder(BST* root, int indent=0)
 {
 	if (root)
 	{
 		if (root->right)
 		{
-			postorder(root->right, indent + 4);
+			BST_postorder(root->right, indent + 4);
 		}
 		if (indent) 
 		{
@@ -59,7 +124,7 @@ void postorder(BST* root, int indent)
 		cout << root->key << "\n ";
 		if (root->left) {
 			cout << setw(indent) << ' ' << " \\\n";
-			postorder(root->left, indent + 4);
+			BST_postorder(root->left, indent + 4);
 		}
 	}
 }
